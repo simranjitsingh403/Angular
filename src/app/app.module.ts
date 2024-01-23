fe353a4b6569c670ad3a6d2855483d708f9ffd8d
simranjitsingh403 <simranjitsingh403@gmail.com>
2024-01-23 12:01:39 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,64 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+// icons
+import { TablerIconsModule } from 'angular-tabler-icons';
+import * as TablerIcons from 'angular-tabler-icons/icons';
+
+//Import all material modules
+import { MaterialModule } from './material.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+//Import Layouts
+import { FullComponent } from './layouts/full/full.component';
+import { BlankComponent } from './layouts/blank/blank.component';
+
+// Vertical Layout
+import { SidebarComponent } from './layouts/full/sidebar/sidebar.component';
+import { HeaderComponent } from './layouts/full/header/header.component';
+import { BrandingComponent } from './layouts/full/sidebar/branding.component';
+import { AppNavItemComponent } from './layouts/full/sidebar/nav-item/nav-item.component';
+import { JwtModule } from '@auth0/angular-jwt';
+import { ToastrModule } from 'ngx-toastr';
+export function tokenGetter() {
+  return localStorage.getItem("access_token");
+}
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FullComponent,
+    BlankComponent,
+    SidebarComponent,
+    HeaderComponent,
+    BrandingComponent,
+    AppNavItemComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MaterialModule,
+    TablerIconsModule.pick(TablerIcons),
+    JwtModule.forRoot({
+      config: {
+        tokenGetter: tokenGetter,
+        allowedDomains: ["example.com"],
+        disallowedRoutes: ["http://example.com/examplebadroute/"],
+      },
+    }),
+    BrowserAnimationsModule, // required animations module
+    ToastrModule.forRoot(),
+  ],
+  exports: [TablerIconsModule],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ src/app/pages/authentication/register/register.component.ts
@@ -0,0 +1,75 @@
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
+import { NavService } from 'src/app/services/nav.service';
+import Validation from 'src/app/utils/validation';
+import { ToastrService } from 'ngx-toastr';
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class AppSideRegisterComponent {
+  constructor(private router: Router, private navService:NavService,private formBuilder: FormBuilder,private toastr: ToastrService) {}
+ data:any;
+ submitted = false;
+
+  form: FormGroup = new FormGroup({
+    userName: new FormControl(''),
+    email: new FormControl(''),
+    password: new FormControl(''),
+    confirmPassword: new FormControl('')
+  });
+  ngOnInit(): void {
+    this.form = this.formBuilder.group(
+      {
+        userName: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(6),
+            Validators.maxLength(20),
+          ],
+        ],
+        email: ['', [Validators.required, Validators.email]],
+        password: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(6),
+            Validators.maxLength(40),
+          ],
+        ],
+        confirmPassword: ['', Validators.required],
+      },
+      {
+        validators: [Validation.match('password', 'confirmPassword')],
+      }
+    );
+  }
+
+  get f() {
+    return this.form.controls;
+  }
+
+  submit() {
+    // console.log(this.form.value);
+    this.router.navigate(['/dashboard']);
+  }
+  signUp()
+  {
+    this.submitted = true;
+    if (this.form.invalid) {
+      return;
+    }
+
+    this.navService.post("Account",this.form.value).subscribe({
+      next: value => console.log(value),
+      error: err => console.error('Observable emitted an error: ' + err),
+      complete: () =>this.toastr.success("Record saved successfully.")
+    });
+
+  }
+
+   
+}
