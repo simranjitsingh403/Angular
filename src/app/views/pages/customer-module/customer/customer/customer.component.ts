4ce0606977842a60d692b83bc798ef12876e473e
simranjitsingh403 <simranjitsingh403@gmail.com>
2024-02-21 18:43:24 +0530

    changes

--- src/app/views/pages/customer-module/customer/customer/customer.component.ts
+++ src/app/views/pages/customer-module/customer/customer/customer.component.ts
@@ -7,6 +7,7 @@ import { environment } from 'src/environments/environment';
 import { ShipmentComponent } from '../../../shipment/shipment/shipment.component';
 import { Customermodel } from 'src/app/model/customermodel';
 import { Usermodel } from 'src/app/model/usermodel';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-customer',
@@ -18,21 +19,21 @@ export class CustomerComponent implements OnInit {
   isFormSubmitted: Boolean;
   apiPath: string = environment.baseURL;
   @ViewChild(ShipmentComponent) shipmentComponent: ShipmentComponent;
-  result:Customermodel = new Customermodel();
-  isLogin=localStorage.getItem('isLoggedin') == null ? false : true;
-  userdetails:Usermodel = new Usermodel();
-  
-  logo = "/assets/images/OneLift_black.png";
-  customerId: any = this.route.snapshot.params['id'] == undefined? "00000000-0000-0000-0000-000000000000" : this.route.snapshot.params['id'];
+  result: Customermodel = new Customermodel();
+  isLogin = localStorage.getItem('isLoggedin') == null ? false : true;
+  userdetails: Usermodel = new Usermodel();
 
-  constructor(public formBuilder: UntypedFormBuilder, private navService: ApiService, private toastr: ToastrService, private route: ActivatedRoute, private router: Router) { }
+  logo = "/assets/images/OneLift_black.png";
+  customerId: any = this.route.snapshot.params['id'] == undefined ? "00000000-0000-0000-0000-000000000000" : this.route.snapshot.params['id'];
+
+  constructor(public formBuilder: UntypedFormBuilder, private navService: ApiService, private toastr: ToastrService, private route: ActivatedRoute, private router: Router, private spinnerService: NgxSpinnerService) { }
 
   ngOnInit(): void {
     this.validationForm = this.formBuilder.group({
       firstName: ['', Validators.required],
       lastName: ['', Validators.required],
       middleName: [''],
-      email:['',Validators.required],
+      email: ['', Validators.required],
     });
 
     this.navService.get<Customermodel>("Customer/Customer/Register?Id=" + this.customerId).subscribe((response) => {
@@ -45,7 +46,7 @@ export class CustomerComponent implements OnInit {
         middleName: this.result.middleName,
         email: this.result.email,
       });
-
+      this.spinnerService.show();
     });
 
     this.isFormSubmitted = false;
@@ -55,12 +56,11 @@ export class CustomerComponent implements OnInit {
     return this.validationForm.controls;
   }
 
-  get shipmentForm(){
-    debugger;
+  get shipmentForm() {
     return this.shipmentComponent.validationForm.controls;
   }
-  
-  formSubmit(event:any) {
+
+  formSubmit(event: any) {
     if (this.validationForm.valid && this.shipmentComponent.validationForm.valid) {
       this.result.firstName = this.form.firstName.value;
       this.result.lastName = this.form.lastName.value;
@@ -82,20 +82,23 @@ export class CustomerComponent implements OnInit {
       this.result.shipment.comments = this.shipmentForm.comments.value;
       this.result.shipment.comodity = this.shipmentForm.comodity.value;
       this.result.shipment.jwtToken = localStorage.getItem('token');
-      
+
       if (this.result.id == "00000000-0000-0000-0000-000000000000") {
-        this.navService.post<any>("Customer/Customer/Register", this.result).subscribe(d => { if (d.success == true) { 
-          this.toastr.success("Record saved successfully"); 
-          localStorage.setItem('isLoggedin', 'true');
-          if (localStorage.getItem('isLoggedin')) {
-            localStorage.setItem('token', d.token);
-            localStorage.setItem('userDetails', d.user);
-            this.userdetails = JSON.parse(localStorage.getItem('userDetails') || "{}");
-            this.router.navigate(["/admin/shipper/credit/"+ this.userdetails.customerId]);
-          }
-        } else { this.toastr.error(d.message) } });
+        this.navService.post<any>("Customer/Customer/Register", this.result).subscribe(d => {
+          if (d.success == true) {
+            this.toastr.success("Record saved successfully");
+            localStorage.setItem('isLoggedin', 'true');
+            if (localStorage.getItem('isLoggedin')) {
+              localStorage.setItem('token', d.token);
+              localStorage.setItem('userDetails', d.user);
+              this.userdetails = JSON.parse(localStorage.getItem('userDetails') || "{}");
+              this.router.navigate(["/admin/shipper/credit/" + this.userdetails.customerId]);
+            }
+          } else { this.toastr.error(d.message) };
+          this.spinnerService.show();
+        });
       }
-      
+
     }
     this.isFormSubmitted = true;
     this.shipmentComponent.isFormSubmitted = true;
