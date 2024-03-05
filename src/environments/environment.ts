f6a5432ff8fee538f9e97ba878d6968d9f28b8b9
Prikshit-Kamal <prikshitkamal26@gmail.com>
2024-03-05 16:08:01 +0530

    ch

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,6 +8,7 @@ export const environment = {
   baseURL:"https://truckloadapi.azurewebsites.net/"
 };
 
+
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
